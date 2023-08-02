2b0d5baaca8098908978a83f62d0fc82b3e6a6cd
Levan <levanmoseshvili5@gmail.com>
2023-08-02 15:51:31 +0400

    finished styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import './App.css'
-import { ResultBox } from './ResultBox'
-import { TextArea } from './TextArea'
+import "./App.css";
+import { ResultBox } from "./ResultBox";
+import { TextArea } from "./TextArea";
 
 function App() {
-
-
   return (
-    <div className='main-div'>
-      <div className='result-bar'>
-        <ResultBox title="words" value="0" />
-        <ResultBox title="characters" value="0" />
-        <ResultBox title="Sentences" value="0" />
-        <ResultBox title="Paragraphs" value="0" />
-        <ResultBox title="pronouns" value="0" />
-      </div>
-      <TextArea />
-      <div className='bottom-result-box'>
-        <ResultBox title="Average Reading Time" value="0" />
-        <ResultBox title="Longest Word" value="0" />
+    <div className="small-container">
+      <div className="main-div">
+        <div className="result-bar">
+          <ResultBox title="words" value="0" />
+          <ResultBox title="characters" value="0" />
+          <ResultBox title="Sentences" value="0" />
+          <ResultBox title="Paragraphs" value="0" />
+          <ResultBox title="pronouns" value="0" />
+        </div>
+        <TextArea />
+        <div className="bottom-result-box">
+          <ResultBox title="Average Reading Time:" value="-" />
+          <ResultBox title="Longest Word:" value="-" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ResultBox.jsx
+++ src/ResultBox.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 
 const ResultBox = ({ title, value }) => {
+  return (
+    <div className="result-box">
+      <span className="box-title">{title}</span>
+      <span className="box-value">{value}</span>
+    </div>
+  );
+};
 
-    return (<div className='result-box'>
-        <span className='box-title'>{title}</span>
-        <span className='box-value'>{value}</span>
-    </div>)
-}
-
-export { ResultBox }
+export { ResultBox };
