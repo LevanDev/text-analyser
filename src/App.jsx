984787dd519cf5e1b6ea72e45a9ea884fdb59bba
Levan <levanmoseshvili5@gmail.com>
2023-08-02 17:41:00 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import { ResultBox } from "./ResultBox";
-import { TextArea } from "./TextArea";
 
 function App() {
+  const [words, setWords] = useState(0);
+  const [characters, setCharacters] = useState(0);
+  const [sentences, setSentences] = useState(0);
+  const [paragraphs, setParagraphs] = useState(0);
+  const [pronouns, setPronouns] = useState(0);
+
+  const [averageReadingTime, setAverageReadingTime] = useState("-");
+  const [longestWord, setLongestWord] = useState("-");
+
+  const handleOnChange = (e) => {
+    const valueFromTextArea = e.target.value;
+
+    setWords(valueFromTextArea.split(" ").filter(x => x.length > 0).length);
+  };
+
   return (
     <div className="small-container">
       <div className="main-div">
         <div className="result-bar">
-          <ResultBox title="words" value="0" />
-          <ResultBox title="characters" value="0" />
-          <ResultBox title="Sentences" value="0" />
-          <ResultBox title="Paragraphs" value="0" />
-          <ResultBox title="pronouns" value="0" />
+          <ResultBox title="Words" value={words} />
+          <ResultBox title="Characters" value={characters} />
+          <ResultBox title="Sentences" value={sentences} />
+          <ResultBox title="Paragraphs" value={paragraphs} />
+          <ResultBox title="Pronouns" value={pronouns} />
         </div>
-        <TextArea />
+        <textarea
+          className="text-area"
+          onChange={handleOnChange}
+          placeholder="Paste your text here"
+        />
         <div className="bottom-result-box">
-          <ResultBox title="Average Reading Time:" value="-" />
-          <ResultBox title="Longest Word:" value="-" />
+          <ResultBox title="Average Reading Time:" value={averageReadingTime} />
+          <ResultBox title="Longest Word:" value={longestWord} />
         </div>
       </div>
     </div>
--- src/TextArea.jsx
+++
@@ -1,6 +0,0 @@
-const TextArea = () => {
-
-    return <textarea className='text-area' placeholder='paste your text here'></textarea>
-}
-
-export { TextArea }
